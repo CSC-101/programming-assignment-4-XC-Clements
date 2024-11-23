b9ded52c4ceb24054590e2abb9f2ba1c7e43559d
Hikatchus <76414030+Hikatchus@users.noreply.github.com>
2024-11-22 19:41:47 -0800

    Should be all done!!!

--- operations.py
+++ operations.py
@@ -42,7 +42,37 @@ def prettify_county(county:CountyDemographics):
 #FINISHED -- Prints out the contents of the memory file to console
 def display():
     for n in running_list.running_list:
-        print(prettify_county(n))
+        print(n.county + ", " + n.state)
+        print("\tPopulation: {}".format(n.population["2014 Population"]))
+
+        print("\tAge:")
+        print("\t\t<5: {}%".format(n.age['Percent Under 5 Years']))
+        print("\t\t<18: {}%".format(n.age['Percent Under 18 Years']))
+        print("\t\t>65: {}%".format(n.age['Percent 65 and Older']))
+
+        print("\tEducation:")
+        print("\t\t Bachelor's Degree or Higher: {}%".format(n.education["Bachelor's Degree or Higher"]))
+        print("\t\t High School Diploma or Higher: {}%".format(n.education['High School or Higher']))
+
+        print("\tEthnicity:")
+        print("\t\t {}".format(n.ethnicities))
+        print("\t\t American Indian and Alaska Native Alone: {}%".format(n.ethnicities['American Indian and Alaska Native Alone']))
+        print("\t\t Asian Alone: {}%".format(n.ethnicities['Asian Alone']))
+        print("\t\t Black Alone: {}%".format(n.ethnicities['Black Alone']))
+        print("\t\t Hispanic or Latino: {}%".format(n.ethnicities['Hispanic or Latino']))
+        print("\t\t Native Hawaiian and Other Pacific Islander Alone: {}%".format(n.ethnicities['Native Hawaiian and Other Pacific Islander Alone']))
+        print("\t\t Two or More Races: {}%".format(n.ethnicities['Two or More Races']))
+        print("\t\t White Alone: {}%".format(n.ethnicities['White Alone']))
+        print("\t\t White Alone, not Hispanic or Latino: {}%".format(n.ethnicities['White Alone, not Hispanic or Latino']))
+
+        print("\tIncome:")
+        print("\t\t Per Capita Income: {}%".format(n.income['Per Capita Income']))
+        print("\t\t Persons Below Poverty Level: {}%".format(n.income['Persons Below Poverty Level']))
+        print("\t\t Median Household Income: {}%".format(n.income['Median Household Income']))
+
+
+
+
 #FINISHED
 def filter_state(state_abbreviation:str):
     output_list = []
